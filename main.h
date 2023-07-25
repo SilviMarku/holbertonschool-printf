39019ca3fdb156b0c07e62a6091f6359f9b5bd9a
SilviMarku <marku.silvia777@gmail.com>
2023-07-25 02:55:58 -0700

    Main.h

---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ **struct print_func -funcion that produces output according to a format
+ *@format: imput
+ *@print: imput
+ */
+typedef struct print_func
+{
+	char *format;
+	int (*print)(va_list list);
+} print_func;
+
+int _putchar(char c);
+int print_char(va_list char_list);
+int print_string(va_list string_list);
+int print_int(va_list int_list);
+int print_float(va_list float_list);
+int _printf(const char *format, ...);
+#endif
+
