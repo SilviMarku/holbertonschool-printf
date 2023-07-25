08e6a5d54ba2ed6ed24e8573b62274939005ae76
SilviMarku <marku.silvia777@gmail.com>
2023-07-25 08:15:49 -0700

    printf function

--- op_functions.c
+++ op_functions.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- *
+ *print_char - functions that prints char character from va_list
+ *@list: list of arguments
+ *Return: Return  1
  *
  */
 int print_char(va_list char_list)
@@ -12,7 +14,9 @@ int print_char(va_list char_list)
 	return(1);
 }
 /**
- *
+ *print_string - functions that prints string from va_list
+ *@list: list of arguments
+ *Return: string
  *
  */
 int print_string(va_list string_list)
