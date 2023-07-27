aa07bcc2886bcff5c83193616e8d2613c8221ca3
izabela9 <isabelanelaj1@gmail.com>
2023-07-27 03:45:26 -0700

    printf function

--- printf_func.c
+++ printf_func.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
- /**
- *_printf - function that produces output according to a format
+/**
+*_printf - function that produces output according to a format
  *@format: format to be used to print the output
  *Return: the number of characters printed
- */
+*/
 int _printf(const char *format, ...)
 {
 	int i, count = 0;
@@ -36,11 +36,9 @@ int _printf(const char *format, ...)
 			}
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
-		{
 			putchar('%');
 			i++;
 			count += 1;
-		}
 		else
 		{
 			putchar(format[i]);
